335c21511d5b27f5a1c1bb2c62d6ccc258210760
magicmoose <ralf@scardiecat.org>
2016-12-29 09:49:33 -0500

    Try init of chartrepo

---
+++ setup_ci.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+curl -fSL https://github.com/subchen/frep/releases/download/v1.1.0/frep-linux-amd64.zip -o frep.zip
+sudo unzip frep.zip -d /usr/local/bin/
+rm frep.zip
+
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh | bash
